3966064655851427b7713509167a032ad315fa58
Thomas Delacour <thomas@thomasDebian.home>
2015-04-05 03:38:22 -0400

    basic dynamic webpage setup

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var path    = require('path');
-var db      = require('./oracle/connectionDriver.js');
-
-var sql = "SELECT * FROM Business WHERE rownum < 4";
+var express    = require('express');
+var path       = require('path');
+var db         = require('./oracle/connectionDriver.js');
+var bodyparser = require('body-parser');
 
 var app = express();
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade');
+app.use(bodyparser.urlencoded({extended: false}));
 app.get('/home', function(req, res) {
-  db(sql, render, res);
+  render_form(res);
+});
+app.post('/home', function(req, res) {
+  var sql = req.body.query; 
+  db(sql, render_out, res);
 });
 app.listen(process.argv[2]);
 
-var render = function(res, db_res) {
-  res.render('index', {qoutput: db_res.rows});
+var render_form = function(res) {
+  res.render('form');
+}
+
+var render_out = function(res, result) {
+  res.render('form', {out: result.rows});
 }
